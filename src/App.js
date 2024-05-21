994774a17524bdfd68abf452ea58a4dd21215244
aungNyeinChan93 <aungNyeinChan.dev@gmail.com>
2024-05-21 14:20:31 +0630

    add UI and jsonServer/axios for react todo-list

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import List from "./components/List";
+import Form from "./components/Form";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
